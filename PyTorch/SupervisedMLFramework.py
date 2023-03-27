5ae9a5b419200202b30b25ca5c249f08affa1ef2
seadniksic <57331248+seadniksic@users.noreply.github.com>
2023-03-27 17:03:27 -0400

    changed framework interface.  Partially completed implementation.  Still need to add dataset construction component

--- PyTorch/SupervisedMLFramework.py
+++ PyTorch/SupervisedMLFramework.py
@@ -1,24 +1,41 @@
 import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, SequentialSampler
 
 
 class SupervisedMLFramework:
 
-    def __init__(self, model, data, labels, split, k: int, autotune: bool, is_custom_model: bool) -> None:
+    def __init__(self, model, dataset, labels, split, autotune: bool, is_custom_model: bool = True) -> None:
         self.model = model
-        self.data = data
         self.labels = labels
         self.autotune = autotune
         self.train_proportion = split[0]
-        self.validation_proportion = split[1]
-        self.test_proportion = split[2]
+        self.test_proportion = split[1]
         self.data_len = len(labels)
         self.is_custom_model = is_custom_model
-        self.k = k
+        self.dataset = dataset
 
-    def preprocess(self, preprocessing_function) -> None:
-        self.data = preprocessing_function(self.data)
+    def preprocess(self, preprocessing_function, data=None):
+        if data == None:
+            self.data = preprocessing_function(self.data)
+            return None
+        else:
+            return preprocessing_function(data)
 
-    def train(self):
+    def postprocess(self, postprocessing_function) ->  None:
+        self.da
+
+    def train(self,  lr, epochs, loss_function, optim, batch_size=32, k=10):
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        #Move model to GPU if available
+        model = self.model.to(device)
+
+        loss_fn = loss_function()
+        optimizer = optim(model.parameters(), lr=lr)
+
         if self.autotune:
             #Do sklearn gridsearchcv here.  Will need the model to implement the sklearn estimator interface which im not sure about
             pass
@@ -28,39 +45,55 @@ class SupervisedMLFramework:
             #Separate data into train, test
 
                 #Generate indices in random order (no repeats)
-
                 indices = np.random.permutation(range(self.data_len))
-                train_length = int((self.train_proportion + self.validation_proportion)  * self.data_len)
-                train_indices = indices[: train_length]
-                test_indices = indices[train_length:]
+                train_length = int((self.train_proportion)  * self.data_len)
+                self.not_test_indices = indices[: train_length]
+                self.test_indices = indices[train_length:]
+
+                # train_sampler = SubsetRandomSampler(train_indices)
+                # train_loader = DataLoader(self.dataset, batch_size=batch_size, sampler=train_sampler)
                 
+                #Do k fold cross validation on train portion.
+                fold_size = int(len(self.not_test_indices) / k)
+                k_fold_indices = list(range(len(self.not_test_indices)))
 
-                #Take indices and create train / test data and labels
-
-                train_data = self.data[train_indices]
-                train_labels = self.labels[train_indices]
-                test_data = self.data[test_indices]
-                test_labels = self.labels[test_indices]
-
-                #Do k fold cross validation on train portion.  Make sure to shuffle the train set each time.
-                fold_size = int(len(train_data) / self.k)
-                for split in range(1, self.k + 1):
+                for split in range(1, k + 1):
                     #If length of data isn't perfectly divisible by k, give the last fold whatever's left over (a max of k-1 samples) 
-                    k_fold_indices = list(range(train_data))
-                    validation_data = k_fold_indices[(split -1) *fold_size: split *fold_size] if split != self.k else train_data[(split -1) *fold_size:]
+                    validation_indices = k_fold_indices[(split -1) *fold_size: split *fold_size] if split != k else k_fold_indices[(split -1) *fold_size:]
+                    train_indices = list(set(k_fold_indices)  - set(validation_indices))
+
+                    train_sampler = SequentialSampler(self.not_test_indices[train_indices])
+                    validation_sampler = SequentialSampler(self.not_test_indices[validation_indices])
+
+                    train_dataloader = DataLoader(self.dataset, batch_size, sampler=train_sampler)
+                    validation_dataloader = DataLoader(self.dataset, batch_size, sampler=validation_sampler)
+                        
+                    for epoch in epochs:
+
+                        for batch, (X, y) in enumerate(train_dataloader):
+                            prediction = model(X)
+                            loss = loss_fn(prediction, y)
+
+                            #Backprop
+                            optimizer.zero_grad()
+                            loss.backward()
+                            optimizer.step()
+
+                            if batch % 100 == 0:
+                                loss, current = loss.item(), (batch + 1) * len(X)
+                                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
                     
+                        for batch, (X, y) in enumerate(validation_dataloader):
+                            with torch.no_grad():
+                                prediction = model(X)
+                                loss = loss_fn(prediction, y)
 
-                if not self.is_custom_model:
-                    self.model.fit(train_data, train_labels)
-                else:
-                    #TODO
-                    pass
+                                if batch % 100 == 0:
+                                    loss, current = loss.item(), (batch + 1) * len(X)
+                                    print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-
-
-            #Separate train data into 
 
     #TODO
-    def test():
+    def test(self):
         pass
     
