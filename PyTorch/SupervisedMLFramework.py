ed7669dd8dd14b8efb8d3f3abc600b00bea90d32
seadniksic <57331248+seadniksic@users.noreply.github.com>
2023-03-30 09:44:23 -0400

    updated framework to include train, test, tune, and predict. Tested all functions. Integrated Tensorboard support

--- PyTorch/SupervisedMLFramework.py
+++ PyTorch/SupervisedMLFramework.py
@@ -2,120 +2,161 @@ import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, SequentialSampler
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
 
 
 class SupervisedMLFramework:
 
-    def __init__(self, model, dataset, labels, split, autotune: bool, is_custom_model: bool = True) -> None:
+    def __init__(self, model, train_dataset, test_dataset) -> None:
         self.model = model
-        self.labels = labels
-        self.autotune = autotune
-        self.train_proportion = split[0]
-        self.test_proportion = split[1]
-        self.data_len = len(labels)
-        self.is_custom_model = is_custom_model
-        self.dataset = dataset
+        self.train_dataset = train_dataset
+        self.test_dataset = test_dataset
 
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         #Move model to GPU if available
-        self.model = model.to(device)
+        self.model = model.to(self.device)
 
-    # def preprocess(self, preprocessing_function, data=None):
-    #     if data == None:
-    #         self.data = preprocessing_function(self.data)
-    #         return None
-    #     else:
-    #         return preprocessing_function(data)
-
-    # def postprocess(self, postprocessing_function) ->  None:
-    #     self.da
-
-    def train(self,  lr, epochs, loss_function, optim, batch_size=32, k=10):
-
+        self.writer = SummaryWriter()
+    
+    def __del__(self):
+        self.writer.close()
         
-
-        loss_fn = loss_function()
-        optimizer = optim(self.model.parameters(), lr=lr)
-
-        if self.autotune:
-            #Do sklearn gridsearchcv here.  Will need the model to implement the sklearn estimator interface which im not sure about
-            pass
-        else:
-            #Perform k fold cross validation on the model.  Assume default params are already chosen
-
-            #Separate data into train, test
-
-                #Generate indices in random order (no repeats)
-                indices = np.random.permutation(range(self.data_len))
-                train_length = int((self.train_proportion)  * self.data_len)
-                self.not_test_indices = indices[: train_length]
-                self.test_indices = indices[train_length:]
-
-                # train_sampler = SubsetRandomSampler(train_indices)
-                # train_loader = DataLoader(self.dataset, batch_size=batch_size, sampler=train_sampler)
-                
-                #Do k fold cross validation on train portion.
-                fold_size = int(len(self.not_test_indices) / k)
-                k_fold_indices = list(range(len(self.not_test_indices)))
-
-                for split in range(1, k + 1):
-                    #If length of data isn't perfectly divisible by k, give the last fold whatever's left over (a max of k-1 samples) 
-                    validation_indices = k_fold_indices[(split -1) *fold_size: split *fold_size] if split != k else k_fold_indices[(split -1) *fold_size:]
-                    train_indices = list(set(k_fold_indices)  - set(validation_indices))
-
-                    train_sampler = SequentialSampler(self.not_test_indices[train_indices])
-                    validation_sampler = SequentialSampler(self.not_test_indices[validation_indices])
-
-                    train_dataloader = DataLoader(self.dataset, batch_size, sampler=train_sampler)
-                    validation_dataloader = DataLoader(self.dataset, batch_size, sampler=validation_sampler)
-                        
-                    for epoch in range(epochs):
-
-                        for batch, (X, y) in enumerate(train_dataloader):
-                            prediction = self.model(X)
-                            loss = loss_fn(prediction, y)
-
-                            #Backprop
-                            optimizer.zero_grad()
-                            loss.backward()
-                            optimizer.step()
-
-                            if batch % 100 == 0:
-                                loss, current = loss.item(), (batch + 1) * len(X)
-                                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-                    
-                        for batch, (X, y) in enumerate(validation_dataloader):
-                            with torch.no_grad():
-                                prediction = self.model(X)
-                                loss = loss_fn(prediction, y)
-
-                                if batch % 100 == 0:
-                                    loss, current = loss.item(), (batch + 1) * len(X)
-                                    print(f"validation loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-                                
-
-
-    #TODO
     def test(self, loss_function, batch_size=32):
         
-        test_sampler = SequentialSampler(self.test_indices)
-        test_dataloader = DataLoader(self.dataset, batch_size, sampler=test_sampler)
+        test_dataloader = DataLoader(self.test_dataset, batch_size)
 
-        loss_fn = loss_function()
-
-        testing_loss = 0
+        testing_loss, correct = 0, 0
         for batch, (X, y) in enumerate(test_dataloader):
+
+            X = X.to(self.device)
+            y = y.to(self.device)
+
             prediction = self.model(X)
-            loss = loss_fn(prediction, y)
+            loss = loss_function(prediction, y)
+
+            correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
             testing_loss += loss.item()
 
+        print(f"% correct: {correct / len(self.test_dataset)}")
+
+    def train(self,  lr, epochs, loss_function, optim, batch_size=32):
+        
+        optimizer = optim(self.model.parameters(), lr=lr)
+
+        train_dataloader = DataLoader(self.train_dataset, batch_size)
+
+        for epoch in range(epochs):
+            
+            print(f"\n {'-'*10} Epoch {epoch} {'-'*10} ")
+            total_batch_loss = 0
+            for batch, (X, y) in enumerate(train_dataloader):
+
+                X = X.to(self.device)
+                y = y.to(self.device)
+
+                prediction = self.model(X)
+                loss = loss_function(prediction, y)
+
+                #Backprop
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                if batch % 100 == 0:
+                    avg_loss_over_batch, current = loss.item(), (batch + 1) * len(X)
+                    total_batch_loss += avg_loss_over_batch
+                    print(f"loss: {loss:>7f}  [{current:>5d}/{len(self.train_dataset):>5d}]")
+
+            avg_epoch_loss = total_batch_loss / len(train_dataloader)
+
+            self.writer.add_scalar('Loss/train', avg_epoch_loss, epoch)
+     
+    def tune(self,  lr, epochs, loss_function, optim, batch_size=32, k=5):
+
+        optimizer = optim(self.model.parameters(), lr=lr)
+
+        #Do k fold cross validation on train portion.
+        indices = np.random.permutation(range(len(self.train_dataset)))
+        fold_size = int(len(indices) / k)
+        k_fold_indices = list(range(len(indices)))
+
+        split_validation_losses = []
+        for split in range(1, k + 1):
+            #If length of data isn't perfectly divisible by k, give the last fold whatever's left over (a max of k-1 samples) 
+            validation_indices = k_fold_indices[(split -1) *fold_size: split *fold_size] if split != k else k_fold_indices[(split -1) *fold_size:]
+                        
+            train_indices = list(set(k_fold_indices)  - set(validation_indices))
+
+            train_sampler = SequentialSampler(indices[train_indices])
+            validation_sampler = SequentialSampler(indices[validation_indices])
+
+            train_dataloader = DataLoader(self.train_dataset, batch_size, sampler=train_sampler)
+            validation_dataloader = DataLoader(self.train_dataset, batch_size, sampler=validation_sampler)
+            
+            total_validation_loss = 0
+            for epoch in range(epochs):
+                
+                print(f"\n {'-'*10} Epoch {epoch} {'-'*10} ")
+
+                total_train_loss = 0
+                for batch, (X, y) in enumerate(train_dataloader):
+                    
+                    X = X.to(self.device)
+                    y = y.to(self.device)
+
+                    prediction = self.model(X)
+                    loss = loss_function(prediction, y)
+
+                    #Backprop
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    if batch % 100 == 0:
+                        loss, current = loss.item(), (batch + 1) * len(X)
+                        total_train_loss += loss
+                        print(f"loss: {loss:>7f}  [{current:>5d}/{len(train_sampler):>5d}]")
+                
+                average_epoch_train_loss = total_train_loss / len(train_dataloader)
+
+                self.writer.add_scalar('Loss/train', average_epoch_train_loss, epoch)
+
+                print(f"\n {'-'*10} Validating (epoch {epoch}) {'-'*10} ")
+
+                epoch_validation_loss = 0
+                num_correct = 0
+                for batch, (X, y) in enumerate(validation_dataloader):
+                    with torch.no_grad():
+
+                        X = X.to(self.device)
+                        y = y.to(self.device)
+
+                        prediction = self.model(X)
+                        loss = loss_function(prediction, y)
+
+                        num_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
+
+                        if batch % 100 == 0:
+                            loss, current = loss.item(), (batch + 1) * len(X)
+                            epoch_validation_loss += loss
+
+                avg_epoch_validation_loss = epoch_validation_loss / len(validation_dataloader)
+                total_validation_loss += avg_epoch_validation_loss
+                print(f"Average batch validation loss for epoch {epoch}: {avg_epoch_validation_loss}")
+                print(f"Percentage correct in validation pass: {num_correct / len(validation_indices)}")
+
+
+            print(f"Average validation loss over all epochs: {total_validation_loss / epochs}")
+            split_validation_losses.append(total_validation_loss)
+
+        avg_validation_loss_all_splits = sum(split_validation_losses) / len(split_validation_losses)
+        print(f"Average validation loss over all splits: {avg_validation_loss_all_splits}")
 
     def predict(self, sample):
-        
-        if self.post_processing != None:
-            return self.post_processing(self.model(sample))
-        else:
+
+        sample = sample.to(self.device)
+        with torch.no_grad():
             return self.model(sample)
